26c3a0c7110d2a52438988687ecf05cc0882e2d1
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/validatefastq/ValidateFastqTest.scala
+++ src/test/scala/nl/biopet/tools/validatefastq/ValidateFastqTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.validatefastq
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object ValidateFastqTest extends BiopetTest {
+class ValidateFastqTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
